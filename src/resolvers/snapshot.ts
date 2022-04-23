b424e54a6267c49194d49f7223ba46404f2cc50a
midgerate <midgerate@gmail.com>
2022-04-23 08:55:11 +0530

    fix(): use cloudflare gateway for ipfs, use env var for hub host

--- src/resolvers/snapshot.ts
+++ src/resolvers/snapshot.ts
@@ -5,8 +5,7 @@ import { max } from '../constants.json';
 
 export default async function resolve(address) {
   try {
-    // TODO: add correct url for the hub
-    const url = `http://localhost:8000/graphql`;
+    const url = `${process.env.HUB_HOST}/graphql`;
 
     const user = (
       await axios({
@@ -26,9 +25,7 @@ export default async function resolve(address) {
     if (!user) throw new Error('User not found');
     if (!user.avatar) throw new Error('User avatar not set');
 
-    // TODO: how to get the avatar from our own ipfs function
-    // TODO: ipfs is naturally slow, so what should be the strategy as we wait for all the resolvers to resolve.
-    const ipfsUrl = `https://ipfs.io/ipfs/${user.avatar.replace('ipfs://', '')}`;
+    const ipfsUrl = `https://cloudflare-ipfs.com/ipfs/${user.avatar.replace('ipfs://', '')}`;
 
     const input = (await axios({ url: ipfsUrl, responseType: 'arraybuffer' })).data as Buffer;
     return await resize(input, max, max);
