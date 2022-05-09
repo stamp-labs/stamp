ac732e1bf26686d6d9f663d398eabb3e26449d21
FlacoJones <andrew@openq.dev>
2022-05-09 09:18:48 -0500

    removes now unused resolveName method in ens.ts

--- src/resolvers/ens.ts
+++ src/resolvers/ens.ts
@@ -1,26 +1,20 @@
 import axios from 'axios';
 import { StaticJsonRpcProvider } from '@ethersproject/providers';
-import { Contract } from '@ethersproject/contracts';
 import { resize } from '../utils';
 import { max } from '../constants.json';
 
-async function resolveName(network, address) {
-  const provider = new StaticJsonRpcProvider('https://rpc.ankr.com/eth');
-  const abi = ['function getNames(address[]) view returns (string[])'];
-  const contract = new Contract('0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C', abi, provider);
-  const names = await contract.getNames([address]);
-  return names[0];
-}
 
-export default async function resolve(address, network) {
+export default async function resolve(address) {
   try {
     const provider = new StaticJsonRpcProvider('https://rpc.ankr.com/eth');
     const name = await provider.lookupAddress(address);
-    const resolver = await provider.getResolver(name);
-    const avatar = await resolver.getAvatar();
-    const url = avatar.url;
-    const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
-    return await resize(input, max, max);
+    if (name) {
+      const resolver = await provider.getResolver(name);
+      const avatar = await resolver?.getAvatar();
+      const url = avatar?.url;
+      const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
+      return await resize(input, max, max);
+    }
   } catch (e) {
     return false;
   }
