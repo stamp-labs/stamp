dfb6d2e011d32c105d661f67fc3a4ac78833e91c
FlacoJones <andrew@openq.dev>
2022-05-09 10:17:28 -0500

    adds null check for ens name

--- src/resolvers/ens.ts
+++ src/resolvers/ens.ts
@@ -8,13 +8,12 @@ export default async function resolve(address) {
   try {
     const provider = new StaticJsonRpcProvider('https://rpc.ankr.com/eth');
     const name = await provider.lookupAddress(address);
-    if (name) {
-      const resolver = await provider.getResolver(name);
-      const avatar = await resolver?.getAvatar();
-      const url = avatar?.url;
-      const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
-      return await resize(input, max, max);
-    }
+    if (!name) return false;
+    const resolver = await provider.getResolver(name);
+    const avatar = await resolver?.getAvatar();
+    const url = avatar?.url;
+    const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
+    return await resize(input, max, max);
   } catch (e) {
     return false;
   }
