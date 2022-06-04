fe448a37ae0fe5c21017c979e92dbce89d2347a0
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-06-04 18:04:43 +0200

    test: add resolver tests

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/', '<rootDir>/test/fixtures/'],
+
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/', '<rootDir>/test/fixtures/'],
+  moduleFileExtensions: ['js', 'ts']
+};
---
+++ test/unit/trustwallet.test.ts
@@ -0,0 +1,18 @@
+import resolvers from '../../src/resolvers';
+
+describe('resolvers', () => {
+  describe('trustwallet', () => {
+    it('should return false if missing', async () => {
+      const result = await resolvers.trustwallet('0x556B14CbdA79A36dC33FcD461a04A5BCb5dC2A70', '');
+
+      expect(result).toBe(false);
+    });
+
+    it('should resolve', async () => {
+      const result = await resolvers.trustwallet('0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E', '');
+
+      expect(result).toBeInstanceOf(Buffer);
+      expect(result.length).toBeGreaterThan(1000);
+    });
+  });
+});
