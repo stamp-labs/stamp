59df0db63d4347c3829313c2df8375745b9a27da
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-06-04 19:01:27 +0200

    test: add resolver tests (#17)
    * test: add resolver tests
    
    * chore: enable Github Actions
    
    * test: add space resolver tests
    
    * chore: move tests to resolvers dir

---
+++ test/unit/resolvers/snapshot.test.ts
@@ -0,0 +1,18 @@
+import resolvers from '../../../src/resolvers';
+
+describe('resolvers', () => {
+  describe('snapshot', () => {
+    it('should return false if missing', async () => {
+      const result = await resolvers.snapshot('0x556B14CbdA79A36dC33FcD461a04A5BCb5dC2A70');
+
+      expect(result).toBe(false);
+    });
+
+    it('should resolve', async () => {
+      const result = await resolvers.snapshot('0xeF8305E140ac520225DAf050e2f71d5fBcC543e7');
+
+      expect(result).toBeInstanceOf(Buffer);
+      expect(result.length).toBeGreaterThan(1000);
+    });
+  });
+});
