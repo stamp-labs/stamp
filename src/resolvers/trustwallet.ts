a93cc21a3dde92bd6b3b048dcae06ab7df43a8d0
bonustrack <fabien@bonustrack.co>
2022-06-19 20:14:06 +0700

    Add base token image

--- src/resolvers/trustwallet.ts
+++ src/resolvers/trustwallet.ts
@@ -3,6 +3,11 @@ import { getAddress } from '@ethersproject/address';
 import { resize } from '../utils';
 import { max } from '../constants.json';
 
+const ETH = [
+  '0x0000000000000000000000000000000000000000',
+  '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
+];
+
 export default async function resolve(address, network) {
   try {
     let networkStr = 'ethereum';
@@ -11,7 +16,8 @@ export default async function resolve(address, network) {
     if (network === '137') networkStr = 'polygon';
     if (network === '42161') networkStr = 'arbitrum';
     const checksum = getAddress(address);
-    const url = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${networkStr}/assets/${checksum}/logo.png`;
+    let url = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${networkStr}/assets/${checksum}/logo.png`;
+    if (ETH.includes(checksum)) url = 'https://static.cdnlogo.com/logos/e/81/ethereum-eth.svg';
     const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
     return await resize(input, max, max);
   } catch (e) {
