8cece8a21672115edcbf8521d8ca8a969dcbd54c
less <fabien@bonustrack.co>
2023-06-13 15:23:02 +0700

    feat: support multiple network with space-sx

--- src/resolvers/space-sx.ts
+++ src/resolvers/space-sx.ts
@@ -2,22 +2,40 @@ import axios from 'axios';
 import { getUrl, resize } from '../utils';
 import { max } from '../constants.json';
 
-const HUB_URL = 'https://api.thegraph.com/subgraphs/name/snapshot-labs/sx-goerli';
+const SUBGRAPH_URLS = [
+  'https://api.studio.thegraph.com/query/23545/sx-goerli/version/latest',
+  'https://api.studio.thegraph.com/query/23545/sx-sepolia/version/latest',
+  'https://thegraph.goerli.zkevm.consensys.net/subgraphs/name/snapshot-labs/sx-subgraph'
+];
+
+async function getSpaceAvatar(key, url) {
+  const data = await axios({
+    url,
+    method: 'POST',
+    data: {
+      query: `
+        query {
+          space(id: "${key}") {
+            metadata {
+              avatar
+            }
+          }
+        }`
+    }
+  });
+
+  if (!data.data?.data?.space?.metadata?.avatar) return Promise.reject(false);
+
+  return data.data.data.space.metadata?.avatar;
+}
 
 export default async function resolve(key) {
   try {
-    const space = (
-      await axios({
-        url: HUB_URL,
-        method: 'post',
-        data: {
-          query: `query { space(id: "${key}") { avatar } }`
-        }
-      })
-    ).data.data.space;
-    if (!space || !space.avatar) return false;
-    const url = getUrl(space.avatar);
+    const avatar = await Promise.any(SUBGRAPH_URLS.map(url => getSpaceAvatar(key, url)));
+
+    const url = getUrl(avatar);
     const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
+
     return await resize(input, max, max);
   } catch (e) {
     return false;
