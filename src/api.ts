74da86fe4fbfaf4a0d4825fcde5f1e27f7087824
Chaitanya <yourchaitu@gmail.com>
2023-06-14 23:23:51 +0530

    Fix error message (#57)

--- src/api.ts
+++ src/api.ts
@@ -22,8 +22,10 @@ router.get('/clear/:type/:id', async (req, res) => {
     const key = getCacheKey({ type, network, address, w, h, fallback });
     await clear(key);
     res.status(200).json({ status: 'ok' });
-  } catch (e) {
-    res.status(500).json({ status: 'error', error: e });
+  } catch (e: any = {}) {
+    console.log(e);
+    const errorMessage = e?.message || 'Error while clearing cache'
+    res.status(500).json({ status: 'error', error: errorMessage});
   }
 });
 
