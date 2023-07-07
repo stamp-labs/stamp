b3bce262d2e37510aa0df18fe4f892c482cacb70
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-07 17:56:27 +0900

    chore: send errors to sentry

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
 import api from './api';
+import { initLogger, fallbackLogger } from './sentry';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+initLogger(app);
 
 app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
@@ -14,5 +17,7 @@ app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use(compression());
 app.use('/', api);
+
+fallbackLogger(app);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
---
+++ src/sentry.ts
@@ -0,0 +1,31 @@
+import * as Sentry from '@sentry/node';
+import { Express } from 'express';
+
+export function initLogger(app: Express) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Sentry.Integrations.Express({ app }),
+      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+    ],
+
+    tracesSampleRate: parseFloat(process.env.SENTRY_TRACE_SAMPLE_RATE as string)
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+}
+
+export function fallbackLogger(app: Express) {
+  app.use(Sentry.Handlers.errorHandler());
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  app.use(function onError(err: any, req: any, res: any, _: any) {
+    res.statusCode = 500;
+    res.end(`${res.sentry}\n`);
+  });
+}
+
+export function capture(e: any) {
+  Sentry.captureException(e);
+}
