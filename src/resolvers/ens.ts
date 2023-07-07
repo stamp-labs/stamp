5b98fa1213b3eebd8dcf0bac76b0288556138d5a
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-07 16:23:09 +0900

    fix: fix failing ENS resolver

--- src/resolvers/ens.ts
+++ src/resolvers/ens.ts
@@ -1,22 +1,11 @@
 import axios from 'axios';
 import { getProvider, resize } from '../utils';
 import { max } from '../constants.json';
-import { Contract } from '@ethersproject/contracts';
 
-async function resolveName(address) {
-  const provider = getProvider(1);
-  const abi = ['function getNames(address[]) view returns (string[])'];
-  const contract = new Contract('0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C', abi, provider);
-  const names = await contract.getNames([address]);
-  return names[0];
-}
-
-export default async function resolve(address) {
+export default async function resolve(address: string) {
   try {
     const provider = getProvider(1);
-    const name = await resolveName(address);
-    if (!name) return false;
-    const url = await provider.getAvatar(name);
+    const url = await provider.getAvatar(address);
     if (!url) return false;
     const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
     return await resize(input, max, max);
