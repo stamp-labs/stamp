42c1f3cb4517659c34a6b630641a2d14c4d5ad01
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-11 11:26:41 +0900

    chore: enable sentry only on production

--- src/sentry.ts
+++ src/sentry.ts
@@ -2,15 +2,19 @@ import * as Sentry from '@sentry/node';
 import { Express } from 'express';
 
 export function initLogger(app: Express) {
+  if (process.env.NODE_ENV !== 'production') {
+    return;
+  }
+
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     integrations: [
       new Sentry.Integrations.Http({ tracing: true }),
       new Sentry.Integrations.Express({ app }),
-      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
     ],
 
-    tracesSampleRate: parseFloat(process.env.SENTRY_TRACE_SAMPLE_RATE as string)
+    tracesSampleRate: parseFloat(process.env.SENTRY_TRACE_SAMPLE_RATE as string),
   });
 
   app.use(Sentry.Handlers.requestHandler());
