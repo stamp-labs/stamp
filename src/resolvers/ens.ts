f9b290c62c467d08ec6cd20e31be767b792bfdd6
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-22 10:33:08 +0900

    chore: rename misleading argument name

--- src/resolvers/ens.ts
+++ src/resolvers/ens.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { getProvider, resize } from '../utils';
 import { max } from '../constants.json';
 
-export default async function resolve(address: string) {
+export default async function resolve(name: string) {
   try {
     const provider = getProvider(1);
-    const url = await provider.getAvatar(address);
+    const url = await provider.getAvatar(name);
     if (!url) return false;
     const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
     return await resize(input, max, max);
