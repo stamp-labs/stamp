7da6a7ba0383556322101926dbaaec4bdf835e28
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-01 22:55:08 +0900

    fix: always return the raw value from getAvatar()

--- src/resolvers/ens.ts
+++ src/resolvers/ens.ts
@@ -5,9 +5,18 @@ import { max } from '../constants.json';
 export default async function resolve(name: string) {
   try {
     const provider = getProvider(1);
-    const url = await provider.getAvatar(name);
-    if (!url) return false;
+    const ensResolver = await provider.getResolver(name);
+    if (!ensResolver) {
+      return false;
+    }
+
+    const url = await ensResolver.getText('avatar');
+    if (!url) {
+      return false;
+    }
+
     const input = (await axios({ url, responseType: 'arraybuffer' })).data as Buffer;
+
     return await resize(input, max, max);
   } catch (e) {
     return false;
