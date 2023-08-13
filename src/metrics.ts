d7c762533e75079f3bc3ab137ab1da847b65be83
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-13 14:57:16 +0900

    chore: fix regex

--- src/metrics.ts
+++ src/metrics.ts
@@ -7,13 +7,13 @@ const TYPE_CONSTRAINTS = Object.keys(constants.resolvers).join('|');
 export default function initMetrics(app: Express) {
   init(app, {
     normalizedPath: [
-      [`^/clear/(${TYPE_CONSTRAINTS})/.*`, '/clear/$1/#id'],
-      [`^/(${TYPE_CONSTRAINTS})/.*`, '/$1/#id']
+      [`^/clear/(${TYPE_CONSTRAINTS})/.+`, '/clear/$1/#id'],
+      [`^/(${TYPE_CONSTRAINTS})/.+`, '/$1/#id']
     ],
     whitelistedPath: [
       /^\/$/,
-      new RegExp(`^/clear/(${TYPE_CONSTRAINTS})/.*$`),
-      new RegExp(`^/(${TYPE_CONSTRAINTS})/.*$`)
+      new RegExp(`^/clear/(${TYPE_CONSTRAINTS})/.+$`),
+      new RegExp(`^/(${TYPE_CONSTRAINTS})/.+$`)
     ]
   });
 }
