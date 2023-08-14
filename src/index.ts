a94d296a5ac5072fa17d432a7bb3f7c92cabcd37
Wan <495709+wa0x6e@users.noreply.github.com>
2023-08-14 17:58:35 +0900

    feat: add metrics (#81)
    * feat: install metrics
    
    * chore: remove dependabot
    
    * chore: remove powered by express header
    
    * chore: add constraints to router url
    
    Stop processing unrelevant url
    
    * chore: move fallback and root route to index.ts
    
    * chore: add urls to http request metrics
    
    * chore: fix lint issues
    
    * chore: fix regex

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
-import api from './api';
 import { initLogger, fallbackLogger } from '@snapshot-labs/snapshot-sentry';
+import initMetrics from './metrics';
+import api from './api';
+import { name, version } from '../package.json';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 initLogger(app);
+initMetrics(app);
 
+app.disable('x-powered-by');
 app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
-
 app.use(cors({ maxAge: 86400 }));
 app.use(compression());
 app.use('/', api);
 
+app.get('/', (req, res) => {
+  const commit = process.env.COMMIT_HASH ?? undefined;
+  res.json({ name, version, commit });
+});
+
 fallbackLogger(app);
+
+app.use((_, res) => {
+  res.status(400).json({ message: 'Not found' });
+});
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
---
+++ src/metrics.ts
@@ -0,0 +1,19 @@
+import init from '@snapshot-labs/snapshot-metrics';
+import { Express } from 'express';
+import constants from './constants.json';
+
+const TYPE_CONSTRAINTS = Object.keys(constants.resolvers).join('|');
+
+export default function initMetrics(app: Express) {
+  init(app, {
+    normalizedPath: [
+      [`^/clear/(${TYPE_CONSTRAINTS})/.+`, '/clear/$1/#id'],
+      [`^/(${TYPE_CONSTRAINTS})/.+`, '/$1/#id']
+    ],
+    whitelistedPath: [
+      /^\/$/,
+      new RegExp(`^/clear/(${TYPE_CONSTRAINTS})/.+$`),
+      new RegExp(`^/(${TYPE_CONSTRAINTS})/.+$`)
+    ]
+  });
+}
