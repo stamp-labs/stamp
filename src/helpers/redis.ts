a4696e4388a4fbc716b7767ead28fc08ad8e3a3a
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-11-18 00:08:50 +0800

    feat: add redis database

---
+++ src/helpers/redis.ts
@@ -0,0 +1,18 @@
+import { createClient } from 'redis';
+
+let client;
+
+(async () => {
+  if (!process.env.REDIS_URL) return;
+
+  console.log('[redis] Connecting to Redis');
+  client = createClient({ url: process.env.REDIS_URL });
+  client.on('connect', () => console.log('[redis] Redis connect'));
+  client.on('ready', () => console.log('[redis] Redis ready'));
+  client.on('reconnecting', err => console.log('[redis] Redis reconnecting', err));
+  client.on('error', err => console.log('[redis] Redis error', err));
+  client.on('end', err => console.log('[redis] Redis end', err));
+  await client.connect();
+})();
+
+export default client;
