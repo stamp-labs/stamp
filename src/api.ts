2324d4f5914382e819707190b386fef36f9e91b1
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-11-21 17:04:29 +0800

    fix: fix

--- src/api.ts
+++ src/api.ts
@@ -15,14 +15,14 @@ router.post('/', async (req, res) => {
   if (!method) return rpcError(res, 400, 'missing method', id);
   try {
     let result: any = {};
-    if (!Array.isArray(params)) return rpcError(400, 'params must be an array of string', id);
+    if (!Array.isArray(params)) return rpcError(res, 400, 'params must be an array of string', id);
 
     if (method === 'lookup_addresses') result = await lookupAddresses(params);
     else if (method === 'resolve_name') result = await resolveName(params[0]);
     else return rpcError(res, 400, 'invalid method', id);
 
     if (result?.error) return rpcError(res, result.code || 500, result.error, id);
-    return rpcSuccess(res, { address: result[params] }, id);
+    return rpcSuccess(res, { address: result[params[0]] }, id);
   } catch (e) {
     capture(e);
     return rpcError(res, 500, e, id);
