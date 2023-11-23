aa9bbf8cf2809f27ca97622d9de8b6c81c4b9fd8
Chaitanya <yourchaitu@gmail.com>
2023-11-23 20:22:31 +0530

    fix: Some ENS doesn't have resolved address (#129)

--- src/addressResolvers/ens.ts
+++ src/addressResolvers/ens.ts
@@ -62,7 +62,12 @@ export async function resolveNames(handles: Handle[]): Promise<Record<Handle, Ad
       }`
     );
 
-    return Object.fromEntries(items.map(item => [item.name, getAddress(item.resolvedAddress.id)]));
+    return Object.fromEntries(
+      items.map(item => [
+        item.name,
+        item.resolvedAddress ? getAddress(item.resolvedAddress.id) : ''
+      ])
+    );
   } catch (e) {
     capture(e, { handles });
     return {};
