b2e0c838490c148505550ebd96c210f8a2a3102b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-03 23:54:55 +0700

    refactor: DRY

--- src/resolvers/utils.ts
+++ src/resolvers/utils.ts
@@ -2,21 +2,20 @@ import axios from 'axios';
 import http from 'http';
 import https from 'https';
 
-export async function fetchHttpImage(url: string, options = {}): Promise<Buffer> {
-  return (
-    await axios({
-      url,
-      ...{
-        responseType: 'arraybuffer',
-        timeout: 5e3,
-        ...options
-      }
-    })
-  ).data;
-}
-
 export const axiosDefaultParams = {
   httpAgent: new http.Agent({ keepAlive: true }),
   httpsAgent: new https.Agent({ keepAlive: true }),
   timeout: 5e3
 };
+
+export async function fetchHttpImage(url: string): Promise<Buffer> {
+  return (
+    await axios({
+      url,
+      ...{
+        responseType: 'arraybuffer',
+        ...axiosDefaultParams
+      }
+    })
+  ).data;
+}
