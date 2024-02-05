2c75c185d157d5f4a553f6fc1e1b2042173af381
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-05 17:16:23 +0700

    chore: disable tests when cache is not configured

--- test/integration/resolvers/cache.test.ts
+++ test/integration/resolvers/cache.test.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { Readable } from 'stream';
 import Cache from '../../../src/resolvers/cache';
 import { parseQuery } from '../../../src/utils';
-import { set, streamToBuffer } from '../../../src/aws';
+import { isConfigured, set, streamToBuffer } from '../../../src/aws';
 import constants from '../../../src/constants.json';
 
 const image_buffer = fs.readFileSync(path.join(__dirname, '../../fixtures/sample.webp'));
@@ -11,115 +11,119 @@ const image_buffer = fs.readFileSync(path.join(__dirname, '../../fixtures/sample
 describe('image resolver cache', () => {
   let cache: Cache;
 
-  describe('getBaseImage()', () => {
-    afterEach(async () => {
-      await cache.clear();
-    });
-
-    describe('when the image is cached', () => {
-      it('should return the cached image', async () => {
-        const parsedQuery = await parseQuery('0x0-test-getbaseimage', 'avatar', {});
-        cache = new Cache(parsedQuery);
-
-        await set(cache.baseImageCacheKey, image_buffer);
-        const result = await cache.getBaseImage();
-        return expect(streamToBuffer(result as Readable)).resolves.toEqual(image_buffer);
+  if (isConfigured) {
+    it.todo('needs to configure the cache for the tests to run');
+  } else {
+    describe('getBaseImage()', () => {
+      afterEach(async () => {
+        await cache.clear();
       });
-    });
 
-    describe('when the image is not cached', () => {
-      it('should return false', async () => {
-        const parsedQuery = await parseQuery('0x1-test-getbaseimage', 'avatar', {});
-        cache = new Cache(parsedQuery);
+      describe('when the image is cached', () => {
+        it('should return the cached image', async () => {
+          const parsedQuery = await parseQuery('0x0-test-getbaseimage', 'avatar', {});
+          cache = new Cache(parsedQuery);
 
-        return expect(cache.getBaseImage()).resolves.toBe(false);
-      });
-    });
-  });
-
-  describe('getResizedImage()', () => {
-    afterEach(async () => {
-      await cache.clear();
-    });
-
-    describe('when the image is cached', () => {
-      it('should return the cached image', async () => {
-        const parsedQuery = await parseQuery('0x0-test-getresizedimage', 'avatar', {});
-        cache = new Cache(parsedQuery);
-
-        await set(cache.resizedImageCacheKey, image_buffer);
-        const result = await cache.getResizedImage();
-        return expect(streamToBuffer(result as Readable)).resolves.toEqual(image_buffer);
-      });
-    });
-
-    describe('when the image is not cached', () => {
-      it('should return false', async () => {
-        const parsedQuery = await parseQuery('0x1-test-getresizedimage', 'avatar', {});
-        cache = new Cache(parsedQuery);
-
-        return expect(cache.getResizedImage()).resolves.toBe(false);
-      });
-    });
-  });
-
-  describe('setBaseImage', () => {
-    it('should save the image in the cache', async () => {
-      const parsedQuery = await parseQuery('0x0-set-base-image', 'avatar', {});
-      cache = new Cache(parsedQuery);
-
-      return expect(cache.setBaseImage(image_buffer)).resolves.toEqual(
-        expect.objectContaining({
-          $metadata: expect.objectContaining({ httpStatusCode: 200 })
-        })
-      );
-    });
-  });
-
-  describe('setResizedImage()', () => {
-    it('should save the image in the cache', async () => {
-      const parsedQuery = await parseQuery('0x0-set-resized-image', 'avatar', {});
-      cache = new Cache(parsedQuery);
-
-      return expect(cache.setResizedImage(image_buffer)).resolves.toEqual(
-        expect.objectContaining({
-          $metadata: expect.objectContaining({ httpStatusCode: 200 })
-        })
-      );
-    });
-  });
-
-  describe('clear()', () => {
-    describe('when the cache exist', () => {
-      it('should clear the cache', async () => {
-        const parsedQuery = await parseQuery('0x0-clear-exist', 'avatar', {
-          s: constants.max,
-          fb: 'fb-0',
-          cb: 'cb-0'
+          await set(cache.baseImageCacheKey, image_buffer);
+          const result = await cache.getBaseImage();
+          return expect(streamToBuffer(result as Readable)).resolves.toEqual(image_buffer);
         });
-        cache = new Cache(parsedQuery);
-        await cache.setBaseImage(image_buffer);
+      });
 
-        await new Promise(resolve => {
-          setTimeout(resolve, 3e3);
+      describe('when the image is not cached', () => {
+        it('should return false', async () => {
+          const parsedQuery = await parseQuery('0x1-test-getbaseimage', 'avatar', {});
+          cache = new Cache(parsedQuery);
+
+          return expect(cache.getBaseImage()).resolves.toBe(false);
         });
-
-        expect(cache.clear()).resolves.toBe(true);
-        expect(cache.getBaseImage()).resolves.toBe(false);
-        expect(cache.getResizedImage()).resolves.toBe(false);
       });
     });
 
-    describe('when the cache does not exist', () => {
-      it('should return false', async () => {
-        const parsedQuery = await parseQuery('0x0-clear-not-exist', 'avatar', {
-          s: constants.max,
-          fb: 'fb-1',
-          cb: 'cb-1'
+    describe('getResizedImage()', () => {
+      afterEach(async () => {
+        await cache.clear();
+      });
+
+      describe('when the image is cached', () => {
+        it('should return the cached image', async () => {
+          const parsedQuery = await parseQuery('0x0-test-getresizedimage', 'avatar', {});
+          cache = new Cache(parsedQuery);
+
+          await set(cache.resizedImageCacheKey, image_buffer);
+          const result = await cache.getResizedImage();
+          return expect(streamToBuffer(result as Readable)).resolves.toEqual(image_buffer);
         });
-        cache = new Cache(parsedQuery);
-        return expect(cache.clear()).resolves.toBe(false);
+      });
+
+      describe('when the image is not cached', () => {
+        it('should return false', async () => {
+          const parsedQuery = await parseQuery('0x1-test-getresizedimage', 'avatar', {});
+          cache = new Cache(parsedQuery);
+
+          return expect(cache.getResizedImage()).resolves.toBe(false);
+        });
       });
     });
-  });
+
+    describe('setBaseImage', () => {
+      it('should save the image in the cache', async () => {
+        const parsedQuery = await parseQuery('0x0-set-base-image', 'avatar', {});
+        cache = new Cache(parsedQuery);
+
+        return expect(cache.setBaseImage(image_buffer)).resolves.toEqual(
+          expect.objectContaining({
+            $metadata: expect.objectContaining({ httpStatusCode: 200 })
+          })
+        );
+      });
+    });
+
+    describe('setResizedImage()', () => {
+      it('should save the image in the cache', async () => {
+        const parsedQuery = await parseQuery('0x0-set-resized-image', 'avatar', {});
+        cache = new Cache(parsedQuery);
+
+        return expect(cache.setResizedImage(image_buffer)).resolves.toEqual(
+          expect.objectContaining({
+            $metadata: expect.objectContaining({ httpStatusCode: 200 })
+          })
+        );
+      });
+    });
+
+    describe('clear()', () => {
+      describe('when the cache exist', () => {
+        it('should clear the cache', async () => {
+          const parsedQuery = await parseQuery('0x0-clear-exist', 'avatar', {
+            s: constants.max,
+            fb: 'fb-0',
+            cb: 'cb-0'
+          });
+          cache = new Cache(parsedQuery);
+          await cache.setBaseImage(image_buffer);
+
+          await new Promise(resolve => {
+            setTimeout(resolve, 3e3);
+          });
+
+          expect(cache.clear()).resolves.toBe(true);
+          expect(cache.getBaseImage()).resolves.toBe(false);
+          expect(cache.getResizedImage()).resolves.toBe(false);
+        });
+      });
+
+      describe('when the cache does not exist', () => {
+        it('should return false', async () => {
+          const parsedQuery = await parseQuery('0x0-clear-not-exist', 'avatar', {
+            s: constants.max,
+            fb: 'fb-1',
+            cb: 'cb-1'
+          });
+          cache = new Cache(parsedQuery);
+          return expect(cache.clear()).resolves.toBe(false);
+        });
+      });
+    });
+  }
 });
