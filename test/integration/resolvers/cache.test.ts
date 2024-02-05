c41d36e7443509eaf01fb1c100173b9635d52309
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-05 14:50:48 +0700

    chore: add tests skeleton

---
+++ test/integration/resolvers/cache.test.ts
@@ -0,0 +1,21 @@
+describe('image resolver cache', () => {
+  describe('getBaseImage', () => {
+    it.todo('should return the cached image');
+  });
+
+  describe('getResizedImage', () => {
+    it.todo('should return the cached image');
+  });
+
+  describe('setBaseImage', () => {
+    it.todo('should save the image in the cache');
+  });
+
+  describe('setResizedImage', () => {
+    it.todo('should save the image in the cache');
+  });
+
+  describe('clear', () => {
+    it.todo('should clear the cache');
+  });
+});
