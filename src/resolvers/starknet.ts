55bd1511a4fd45ad85807675a467826632a7a056
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-27 16:58:42 +0800

    feat: resolve starknet avatar from address

--- src/resolvers/starknet.ts
+++ src/resolvers/starknet.ts
@@ -3,17 +3,50 @@ import { getUrl, resize } from '../utils';
 import { max } from '../constants.json';
 import { fetchHttpImage, axiosDefaultParams } from './utils';
 
-export default async function resolve(address: string) {
-  try {
-    if (!address.endsWith('.stark')) {
-      throw new Error('Unsupported domain');
-    }
+async function getImageFromDomain(domain: string): Promise<string> {
+  const res = await axios.get(
+    `https://api.starknet.id/domain_to_data?domain=${domain}`,
+    axiosDefaultParams
+  );
+  return res.data.img_url;
+}
 
+async function getImageFromAddress(address: string): Promise<string> {
+  const tokenIdRes = await axios.get(
+    `https://api.starknet.id/addr_to_token_id?addr=${address}`,
+    axiosDefaultParams
+  );
+  const tokenId = tokenIdRes.data.token_id;
+
+  if (tokenId) {
     const res = await axios.get(
-      `https://api.starknet.id/domain_to_data?domain=${address}`,
+      `https://api.starknet.id/id_to_data?id=${tokenId}`,
       axiosDefaultParams
     );
-    const { img_url } = res.data;
+    return res.data.img_url;
+  }
+
+  throw new Error('No image found for starknet address');
+}
+
+export default async function resolve(domainOrAddress: string) {
+  try {
+    let img_url: string | null = null;
+    if (domainOrAddress.includes('.')) {
+      if (!domainOrAddress.endsWith('.stark')) {
+        throw new Error('Unsupported starknet domain');
+      }
+      img_url = await getImageFromDomain(domainOrAddress);
+    } else if (domainOrAddress.startsWith('0x')) {
+      if (!/^0x[a-f0-9]{64}$/i.test(domainOrAddress)) {
+        throw new Error('Invalid starknet address');
+      }
+      img_url = await getImageFromAddress(domainOrAddress);
+    }
+
+    if (!img_url) {
+      throw new Error('No starknet image found');
+    }
 
     const input = await fetchHttpImage(getUrl(img_url));
     return await resize(input, max, max);
