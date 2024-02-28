7944ab8e25bbf2c384136a317dbf5fc6be0c0c99
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-29 00:06:29 +0800

    chore: fix and add more tests

--- test/unit/resolvers/starknet.test.ts
+++ test/unit/resolvers/starknet.test.ts
@@ -6,13 +6,22 @@ describe('resolvers', () => {
       const result = await resolvers.starknet('test-checkpoint.stark');
 
       expect(result).toBe(false);
-    });
+    }, 10e3);
 
-    it('should resolve', async () => {
+    it('should resolve a domain', async () => {
       const result = await resolvers.starknet('checkpoint.stark');
 
       expect(result).toBeInstanceOf(Buffer);
       expect(result.length).toBeGreaterThan(1000);
-    });
+    }, 10e3);
+
+    it('should resolve an address', async () => {
+      const result = await resolvers.starknet(
+        '0x07FF6B17f07c4d83236E3Fc5f94259A19D1Ed41bBCf1822397EA17882E9b038d'
+      );
+
+      expect(result).toBeInstanceOf(Buffer);
+      expect(result.length).toBeGreaterThan(1000);
+    }, 10e3);
   });
 });
