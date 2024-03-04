b4a002db0f676d15e10d8860dda5d2638040996b
Chaitanya <yourchaitu@gmail.com>
2024-03-04 15:59:47 +0530

    Update aws.ts

--- src/aws.ts
+++ src/aws.ts
@@ -2,12 +2,11 @@ import * as AWS from '@aws-sdk/client-s3';
 import { Readable } from 'stream';
 
 let client: AWS.S3;
-const dir = 'stamp-3';
+const dir = 'stamp-4';
 const bucket = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_REGION;
 const endpoint = process.env.AWS_ENDPOINT || undefined;
 if (region) client = new AWS.S3({ region, endpoint });
-const dir = 'stamp-4';
 
 export const isConfigured = !!(bucket && region);
 
