3274d734c52aeb7b267fc3f0c05f7ce4d5d24a9c
codingsh <codingsh@pm.me>
2024-03-31 16:13:03 -0300

    chore(farcaster): change far-users api to neynar

--- src/resolvers/farcaster.ts
+++ src/resolvers/farcaster.ts
@@ -4,32 +4,50 @@ import { getUrl, resize } from '../utils';
 import { max } from '../constants.json';
 import { fetchHttpImage, axiosDefaultParams } from './utils';
 
-const API_URL = 'https://far-users.vercel.app/api';
+const NEYNAR_API_URL = 'https://api.neynar.com/v2/farcaster/user/bulk-by-address';
+const API_KEY = 'NEYNAR_API_DOCS';
 
-export default async function resolve(address) {
+function normalizeAddress(address) {
   try {
-    const { data } = await axios({
-      url: `${API_URL}/graphql`,
-      method: 'post',
-      data: {
-        query: `
-          query GetUsersByAddresses($address: String!) {
-            getUserByAddress(address: ${address}) {
-              fid
-              username
-              pfp_url
-            }
-          }`
-      },
-      ...axiosDefaultParams
+    return isAddress(address) ? getAddress(address) : null;
+  } catch (error) {
+    console.error('Invalid Ethereum address:', address);
+    return null;
+  }
+}
+
+async function fetchUserDetailsByAddress(normalizedAddress) {
+  try {
+    const response = await axios.get(`${NEYNAR_API_URL}?addresses=${normalizedAddress}`, {
+      headers: { Accept: 'application/json', api_key: API_KEY }
     });
 
-    const sourceUrl = data.data.pfp_url;
-    if (!sourceUrl) return false;
+    const user = response.data[normalizedAddress.toLowerCase()]?.[0];
+    return user || null;
+  } catch (error) {
+    console.error('Error fetching user details:', error);
+    return null;
+  }
+}
 
-    const url = getUrl(sourceUrl);
-    const input = await fetchHttpImage(url);
-    return await resize(input, max, max);
+export default async function resolve(address) {
+  const normalizedAddress = normalizeAddress(address);
+  if (!normalizedAddress) {
+    console.error('Failed to normalize address:', address);
+    return false;
+  }
+
+  const userDetails = await fetchUserDetailsByAddress(normalizedAddress);
+  if (!userDetails || !userDetails.pfp_url) {
+    console.error('No user or profile picture found for address:', address);
+    return false;
+  }
+
+  try {
+    const imageUrl = new URL(userDetails.pfp_url);
+    const imageBlob = await fetchHttpImage(imageUrl.toString());
+    const resizedImage = await resize(imageBlob, max, max);
+    return resizedImage;
   } catch (e) {
     return false;
   }
