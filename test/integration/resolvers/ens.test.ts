712d0d1cd3974e45262d7b38faea59deb1fb25c1
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-05-29 16:53:52 +0400

    chore: fix test not shutting down properly

--- test/integration/resolvers/ens.test.ts
+++ test/integration/resolvers/ens.test.ts
@@ -5,20 +5,20 @@ describe('resolvers', () => {
     it('should return false if avatar is not set', async () => {
       const result = await resolvers.ens('0x556B14CbdA79A36dC33FcD461a04A5BCb5dC2A70');
 
-      expect(result).toBe(false);
+      return expect(result).toBe(false);
     });
 
     it('should return false on invalid ENS name', async () => {
       const result = await resolvers.ens('snapshot-test.eth');
 
-      expect(result).toBe(false);
+      return expect(result).toBe(false);
     }, 10e3);
 
     it('should resolve', async () => {
       const result = await resolvers.ens('snowowl.eth');
 
       expect(result).toBeInstanceOf(Buffer);
-      expect(result.length).toBeGreaterThan(1000);
-    }, 30000);
+      return expect(result.length).toBeGreaterThan(1000);
+    }, 30e3);
   });
 });
