ad09456736f322aea8c5774b312ae94788870feb
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-06-13 19:49:50 +0400

    feat: add support for user cover

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,7 +2,7 @@ import blockie from './blockie';
 import jazzicon from './jazzicon';
 import ens from './ens';
 import trustwallet from './trustwallet';
-import snapshot from './snapshot';
+import { resolveAvatar as sResolveAvatar, resolveCover as sResolveCover } from './snapshot';
 import space from './space';
 import { resolveAvatar as sxResolveAvatar, resolveCover as sxResolveCover } from './space-sx';
 import selfid from './selfid';
@@ -16,7 +16,8 @@ export default {
   jazzicon,
   ens,
   trustwallet,
-  snapshot,
+  snapshot: sResolveAvatar,
+  'user-cover': sResolveCover,
   space,
   'space-sx': sxResolveAvatar,
   'space-cover-sx': sxResolveCover,
