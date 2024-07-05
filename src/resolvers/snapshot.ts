7d710d6166db23c07b5b1386c7ce0f6529bcced2
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-05 14:59:09 +0900

    fix: use address formatter from SDK

--- src/resolvers/snapshot.ts
+++ src/resolvers/snapshot.ts
@@ -1,8 +1,8 @@
+import snapshot from '@snapshot-labs/snapshot.js';
 import axios from 'axios';
 import { getUrl, resize } from '../utils';
 import { max } from '../constants.json';
 import { fetchHttpImage, axiosDefaultParams } from './utils';
-import { getChecksummedAddress } from '../utils';
 
 const HUB_URL = process.env.HUB_URL ?? 'https://hub.snapshot.org';
 
@@ -14,7 +14,9 @@ function createPropertyResolver(property: 'avatar' | 'cover') {
           url: `${HUB_URL}/graphql`,
           method: 'post',
           data: {
-            query: `query { user(id: "${getChecksummedAddress(address)}") { ${property} } }`
+            query: `query { user(id: "${snapshot.utils.getFormattedAddress(
+              address
+            )}") { ${property} } }`
           },
           ...axiosDefaultParams
         })
