7c589704638777326f7a8edf5efdad3df8754dc3
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-10-08 19:02:03 +0400

    feat: add testnet support to snapshot resolver
    test: add tests

--- src/resolvers/snapshot.ts
+++ src/resolvers/snapshot.ts
@@ -1,34 +1,48 @@
 import { getUrl, graphQlCall, resize } from '../utils';
-import { max } from '../constants.json';
+import { max, offchainNetworks, defaultOffchainNetwork } from '../constants.json';
 import { fetchHttpImage } from './utils';
 
-const HUB_URL = process.env.HUB_URL ?? 'https://hub.snapshot.org';
+const HUB_URLS = {
+  s: process.env.HUB_URL ?? 'https://hub.snapshot.org',
+  's-tn': process.env.HUB_URL_TN ?? 'https://testnet.hub.snapshot.org'
+};
+
+async function getOffchainProperty(
+  network: string,
+  id: string,
+  entity: 'user' | 'space',
+  property: 'avatar' | 'cover'
+) {
+  try {
+    const {
+      data: {
+        data: { entry }
+      }
+    } = await graphQlCall(
+      `${HUB_URLS[network]}/graphql`,
+      `query { entry: ${entity}(id: "${id}") { ${property} } }`,
+      {
+        headers: { 'x-api-key': process.env.HUB_API_KEY }
+      }
+    );
+
+    if (!entry?.[property]) return false;
+
+    const url = getUrl(entry[property]);
+    const input = await fetchHttpImage(url);
+
+    if (property === 'cover') return input;
+
+    return await resize(input, max, max);
+  } catch (e) {
+    return false;
+  }
+}
 
 function createPropertyResolver(entity: 'user' | 'space', property: 'avatar' | 'cover') {
-  return async (address: string) => {
-    try {
-      const {
-        data: {
-          data: { entry }
-        }
-      } = await graphQlCall(
-        `${HUB_URL}/graphql`,
-        `query { entry: ${entity}(id: "${address}") { ${property} } }`,
-        {
-          headers: { 'x-api-key': process.env.HUB_API_KEY }
-        }
-      );
-
-      if (!entry?.[property]) return false;
-
-      const url = getUrl(entry[property]);
-      const input = await fetchHttpImage(url);
-
-      if (property === 'cover') return input;
-
-      return await resize(input, max, max);
-    } catch (e) {
-      return false;
+  return async (address: string, network = defaultOffchainNetwork) => {
+    if (offchainNetworks.includes(network)) {
+      return getOffchainProperty(network, address, entity, property);
     }
   };
 }
