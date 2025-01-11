4bd53ea743071cac531d8b1f9114729abe2b4f95
Less <bonustrack@users.noreply.github.com>
2025-01-11 12:13:35 +0100

    feat: add CoinGecko token image resolver (#333)
    * feat: add CoinGecko token image resolver
    
    * refactor: pass the API key via header instead of query string
    
    * refactor: improve types
    
    * perf: return early when chainId is not supported
    
    ---------
    
    Co-authored-by: Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>

---
+++ src/resolvers/coingecko.ts
@@ -0,0 +1,35 @@
+import { getAddress } from '@ethersproject/address';
+import { resize } from '../utils';
+import { max } from '../constants.json';
+import { fetchHttpImage } from './utils';
+
+const API_KEY = process.env.COINGECKO_API_KEY;
+
+const COINGECKO_ASSET_PLATFORMS = {
+  1: 'ethereum',
+  10: 'optimistic-ethereum',
+  137: 'polygon-pos',
+  8453: 'base',
+  42161: 'arbitrum-one'
+};
+
+export default async function resolve(address: string, chainId: string) {
+  if (!API_KEY) return false;
+
+  try {
+    const assetPlatformId = COINGECKO_ASSET_PLATFORMS[chainId];
+
+    if (!assetPlatformId) return false;
+
+    const checksum = getAddress(address);
+    const url = `https://pro-api.coingecko.com/api/v3/coins/${assetPlatformId}/contract/${checksum}`;
+
+    const data = await fetch(url, { headers: { 'x-cg-pro-api-key': API_KEY } }).then(res =>
+      res.json()
+    );
+    const input = await fetchHttpImage(data?.image?.large);
+    return await resize(input, max, max);
+  } catch (e) {
+    return false;
+  }
+}
